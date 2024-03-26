6728e55c92ab14a9924f71bbf07a13aec62ce381
neo <necmibkural@gmail.com>
2024-03-26 12:19:33 +0300

    Update WordsListPage with nullable floating action button and empty list UI
    The changes include:
    1. Adding a null check for the words list before initializing the FloatingActionButton.
    2. Displaying a "No words to show" message when the words list is empty.
    3. Updating the ListView.builder to only be created when words are present.
    
    Binary files and locks have also been changed, possibly due to Gradle caching or file system operations.

--- lib/screens/words_list_page.dart
+++ lib/screens/words_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learningapp/database_helper.dart';
+
 import '../model/word.dart';
 import 'quiz_page.dart';
 
@@ -16,32 +17,42 @@ class WordsListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Words List', style: TextStyle(fontSize: 32)),
       ),
-      body: ListView.builder(
-        itemCount: words.length,
-        itemBuilder: (context, index) {
-          final word = words[index];
-          return Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('${word.english} - ${word.korean}',
-                  style: const TextStyle(fontSize: 24)),
+      body: words.isNotEmpty
+          ? ListView.builder(
+              itemCount: words.length,
+              itemBuilder: (context, index) {
+                final word = words[index];
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('${word.english} - ${word.korean}',
+                        style: const TextStyle(fontSize: 24)),
+                  ),
+                );
+              },
+            )
+          : const Center(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("No words to show"),
+              ),
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.play_arrow),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => QuizPage(
+      floatingActionButton: words.isNotEmpty
+          ? FloatingActionButton(
+              child: const Icon(Icons.play_arrow),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => QuizPage(
                       words: words,
                       dbHelper: dbHelper,
-                    )),
-          );
-        },
-      ),
+                    ),
+                  ),
+                );
+              },
+            )
+          : null,
     );
   }
 }
